8c8e02500872a193200afc2a8d930af001af0ece
Ulyana Hanush <ulyana.hanush@gmail.com>
2024-12-17 16:41:59 +0300

    add Constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by ulyana on 17.12.24.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "_b2C1-RnRhbO9IiFCKiYubFemfG7B7HR3g5akaaOF6o"
+    static let secretKey = "h5pJZWlIvtVyXNhavixtMUEmSKCzX4vU4jYTP5S6vH0"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
